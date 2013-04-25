3ccc95b8fce874bd4dd226adffc50a99fa576482
JesseH <comp@comp-Latitude-E5500.(none)>
2013-04-25 19:02:26 -0400

    lua 5.2 env compatability

--- missions/lib/agent.lua
+++ missions/lib/agent.lua
@@ -1,3 +1,16 @@
+setfenv = setfenv or function(f, t)
+    f = (type(f) == 'function' and f or debug.getinfo(f + 1, 'f').func)
+    local name
+    local up = 0
+    repeat
+        up = up + 1
+        name = debug.getupvalue(f, up)
+    until name == '_ENV' or name == nil
+    if name then
+      debug.upvaluejoin(f, up, function() return t end, 1) -- use unique upvalue, set it to f
+    end
+end
+
 local ansicolors = require 'lib.ansicolors'
 
 local function cwrite(str)
